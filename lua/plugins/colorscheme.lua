f92945a93ba1bb3a80586f66767ef3cb33571e62
xieen <xieenenenen@outlook.com>
2024-08-27 16:31:42 +0800

    change transparent

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -5,7 +5,7 @@ return {
     priority = 1000,
     opts = {
       -- Enable transparent background
-      transparent = true,
+      transparent = false,
 
       -- Enable italics comments
       italic_comments = true,
