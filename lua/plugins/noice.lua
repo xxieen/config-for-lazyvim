05e86f0f2a11034fa212a269822abc2d3fa7b141
xieen <xieenenenen@outlook.com>
2024-08-27 20:57:33 +0800

    disable spell checking in markdown

---
+++ lua/plugins/noice.lua
@@ -0,0 +1,12 @@
+return {
+  "folke/noice.nvim",
+  opts = {
+    presets = {
+      inc_rename = true, -- enables an input dialog for inc-rename.nvim
+      command_palette = false, -- position the cmdline and popupmenu together
+    },
+    cmdline = {
+      view = "cmdline",
+    },
+  },
+}
